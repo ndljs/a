ee12a924c9d938df2c6d0b14fab132da96629282
ndljs <75339670+ndljs@users.noreply.github.com>
2021-02-28 01:47:55 +0100

    Update k.js

--- k.js
+++ k.js
@@ -1,2 +1,2 @@
-{"prea":true,"preb":true,"prec":true,"prez":true,"preq":true,"preh":"","prei":"","pres":"","preo":"","pref":false,"preu":1,"prep":"","prex":"","prek":"","pem":"","prel":2,"ree":"com.tencent.ig",
+{"prea":true,"preb":true,"prec":true,"prez":false,"preq":true,"preh":"","prei":"","pres":"","preo":"","pref":false,"preu":1,"prep":"","prex":"","prek":"","pem":"","prel":2,"ree":"com.tencent.ig",
 "tee":"com.tencent.iglite"}
