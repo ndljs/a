8752ee2a5f9782f9d0ee2c90c271ef898fe10eb5
ndljs <75339670+ndljs@users.noreply.github.com>
2021-03-01 13:57:25 +0100

    Update c.js

--- c.js
+++ c.js
@@ -1,1 +1,1 @@
-{"prea":true,"preb":true,"prec":true,"prez":true,"preq":true,"preh":"","prei":"","pres":"","preo":"","pref":true,"preu":2,"prep":"","prex":"https://bit.ly/WhatsDeleted","prek":"https://bit.ly/WhatsDeleted","pem":"WhatsDeleted: best app to recove deleted messages ,photos and videos"}
+{"prea":true,"preb":true,"prec":true,"prez":true,"preq":true,"preh":"","prei":"","pres":"","preo":"","pref":false,"preu":1,"prep":"","prex":"","prek":"","pem":""}
